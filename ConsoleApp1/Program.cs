732ec681fcae50ba10e6a676d79462aa323619ad
Vova <vovamitsura@gmail.com>
2022-10-07 13:01:10 +0300

    some refactor

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,8 +6,9 @@ class Program
 {
     public static void Main(string[] args)
     {
-        GameAccount gameAccount1 = new GameAccount("1", 3);
-        GameAccount gameAccount2 = new GameAccount("2", 3);
+        var gameAccount1 = new GameAccount("1", 3);
+        var gameAccount2 = new GameAccount("2", 3);
+        
         Console.WriteLine("Current rate of " + gameAccount1.UserName + ": " + gameAccount1.CurrentRating);
         Console.WriteLine("Current rate of " + gameAccount2.UserName + ": " + gameAccount2.CurrentRating);
 
--- ConsoleApp1/game/GameAccount.cs
+++ ConsoleApp1/game/GameAccount.cs
@@ -2,16 +2,16 @@
 
 public class GameAccount
 {
-     public static decimal TotalGameCount;
+    public static decimal TotalGameCount;
     public string UserName { get; set; }
     public short CurrentRating { get; set; }
-    private List<Game> gameList;
+    private List<Game> _gameList;
 
     public GameAccount(string userName, short currentRating)
     {
         UserName = userName;
         CurrentRating = currentRating;
-        gameList = new List<Game>();
+        _gameList = new List<Game>();
     }
 
     private void WinGame(string opponentName, short rating)
@@ -21,14 +21,14 @@ public class GameAccount
             throw new ArgumentException(nameof(rating), "Rating cannot be lower 1");
         }
 
-        if (CurrentRating + rating < 32767)
+        if (CurrentRating + 1 < 32767)
         {
             this.CurrentRating++;
         }
         else
             this.CurrentRating = 32767;
 
-        gameList.Add(new Game(this.UserName, opponentName, rating));
+        _gameList.Add(new Game(this.UserName, opponentName, rating));
     }
 
     private void LoseGame(string opponentName, short rating)
@@ -45,7 +45,7 @@ public class GameAccount
         else
             this.CurrentRating = 1;
 
-        gameList.Add(new Game(opponentName, UserName, rating));
+        _gameList.Add(new Game(opponentName, UserName, rating));
     }
 
     public void Play(GameAccount secondOpponent, short bidRate)
@@ -55,36 +55,36 @@ public class GameAccount
             throw new ArgumentException(nameof(bidRate), "Bid rate cannot be negative");
         }
 
-        int firstUserCloseToWin = Math.Abs(this.CurrentRating - bidRate);
-        int secondUserCloseToWin = Math.Abs(secondOpponent.CurrentRating - bidRate);
+        var firstUserCloseToWin = Math.Abs(this.CurrentRating - bidRate);
+        var secondUserCloseToWin = Math.Abs(secondOpponent.CurrentRating - bidRate);
 
         if (firstUserCloseToWin < secondUserCloseToWin)
         {
-            SetWinner(this, secondOpponent, bidRate);
+            SetWinnerAndLoser(this, secondOpponent, bidRate);
         }
         else if (firstUserCloseToWin > secondUserCloseToWin)
         {
-            SetWinner(secondOpponent, this, bidRate);
+            SetWinnerAndLoser(secondOpponent, this, bidRate);
         }
         else
         {
             Random random = new Random();
-            int winner = (int)random.NextInt64(0, 2);
+            var winner = (int)random.NextInt64(0, 2);
 
             if (winner == 0)
             {
-                SetWinner(this, secondOpponent, bidRate);
+                SetWinnerAndLoser(this, secondOpponent, bidRate);
             }
             else
             {
-                SetWinner(secondOpponent, this, bidRate);
+                SetWinnerAndLoser(secondOpponent, this, bidRate);
             }
         }
 
         TotalGameCount++;
     }
 
-    private void SetWinner(GameAccount opponent1, GameAccount opponent2, short rate)
+    private void SetWinnerAndLoser(GameAccount opponent1, GameAccount opponent2, short rate)
     {
         opponent1.WinGame(opponent2.UserName, rate);
         opponent2.LoseGame(opponent1.UserName, rate);
@@ -97,11 +97,11 @@ public class GameAccount
         report.AppendLine("Game Number\tWinner\tLoser\tBit Rate");
 
 
-        for (int i = 0; i < gameList.Count; i++)
+        for (var i = 0; i < _gameList.Count; i++)
         {
-            report.AppendLine($"{i + 1}\t\t{gameList[i].FirstOpponent}" +
-                              $"\t{gameList[i].SecondOpponent}\t" +
-                              $"{gameList[i].GameRate}");
+            report.AppendLine($"{i + 1}\t\t{_gameList[i].FirstOpponent}" +
+                              $"\t{_gameList[i].SecondOpponent}\t" +
+                              $"{_gameList[i].GameRate}");
         }
 
         return report.ToString();
